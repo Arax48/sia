99f72a7a568fccb659bf6be82bfa555d48d3b1c8
Arax48 <159503056+Arax48@users.noreply.github.com>
2024-02-23 23:12:05 -0500

    Update Sia.java

--- src/sia/Sia.java
+++ src/sia/Sia.java
@@ -1,59 +1,65 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package sia;
+ */
 
 public class Sia {
     // Variables de clase
-    public static int nextId = 1;
-    public static String schoolName = "Universidad de Ejemplo";
+    public static String correoadministrativo;
+    public static float papa_inicial = 5.0f ;
+    public static String nombreUniversidad = "Universidad Nacional de Colombia";
 
     // Variables de objeto
-    public String firstName;
-    public String lastName;
-    public int id;
-    public String program;
-    public double tuitionFees;
+    public String nombre;
+    public String programaCurricular;
+    public String asignaturasInscritas;
+    public float puntajeMatricula;
+    public double valorMatricula;
 
     // Constructor
-    public Sia(String firstName, String lastName, String program, double tuitionFees) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.id = nextId++;
-        this.program = program;
-        this.tuitionFees = tuitionFees;
+    public Sia(String nombre, String programaCurricular, String asignaturasInscritas, float puntajeMatricula, double valorMatricula) {
+        this.nombre = nombre;
+        this.programaCurricular = programaCurricular;
+        this.asignaturasInscritas = asignaturasInscritas;
+        this.programaCurricular = programaCurricular;
+        this.valorMatricula = valorMatricula;
     }
 
     // Método para mostrar el registro de estudiante
-    public void showStudentRecord() {
+    public void mostrarRegistro() {
         System.out.println("Registro de estudiante:");
-        System.out.println("Nombre: " + firstName + " " + lastName);
-        System.out.println("ID: " + id);
-        System.out.println("Programa: " + program);
-        System.out.println("Cuota de matrícula: " + tuitionFees);
-        System.out.println("Escuela: " + schoolName);
+
+        System.out.println(nombreUniversidad);
+
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Programa Curricular: " + programaCurricular);
+        System.out.println("Asignaturas inscritas: " + asignaturasInscritas);
+        System.out.println("Puntaje de matrícula: " + puntajeMatricula );
+        System.out.println("Cuota de matrícula: " + valorMatricula );
+        
     }
 
     // Prueba de la clase Student
     public static void main(String[] args) {
         // Declaración de variables y creación de objetos
-        String firstName1 = "Juan";
-        String lastName1 = "Pérez";
-        String program1 = "Ingeniería de Software";
-        double tuitionFees1 = 25000;
+        String nombre1 = "Alejandro Rodríguez";
+        String programa1 = "Ingeniería de Sistemas";
+        String asignaturasInscritas1 = "POO, Cálculo, Álgebra, Física"; 
+        float puntajeMatricula1 = 35.6f;
+        double valorMatricula1 = 2_500_000;
 
-        String firstName2 = "María";
-        String lastName2 = "García";
-        String program2 = "Ciencias de la Computación";
-        double tuitionFees2 = 22000;
+        String nombre2 = "Maria Alfaro";
+        String programa2 = "Artes";
+        String asignaturasInscritas2 = "Dibujo, Animación, Perspectiva, Anatomía"; 
+        float puntajeMatricula2 = 82.4f;
+        double valorMatricula2 = 9_100_000.65;
 
-        // Creación de objetos Student
-        Sia student1 = new Sia(firstName1, lastName1, program1, tuitionFees1);
-        Sia student2 = new Sia(firstName2, lastName2, program2, tuitionFees2);
+        // Creación de objetos 
+        Sia estudiante1 = new Sia(nombre1, programa1, asignaturasInscritas1, puntajeMatricula1, valorMatricula1);
+        Sia estudiante2 = new Sia(nombre2, programa2, asignaturasInscritas2, puntajeMatricula2, valorMatricula2);
 
         // Mostrar el registro de estudiantes
-        student1.showStudentRecord();
-        student2.showStudentRecord();
+       estudiante1.mostrarRegistro();
+        estudiante2.mostrarRegistro();
     }
 }
